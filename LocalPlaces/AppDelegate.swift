88fb1fba65628b4ef306120c5c0a0b04be800575
Bruno Werminghoff <brunowerminghoff@gmail.com>
2020-03-07 14:58:08 -0300

    Added blank project

---
+++ LocalPlaces/AppDelegate.swift
@@ -0,0 +1,31 @@
+//
+//  AppDelegate.swift
+//  LocalPlaces
+//
+//  Created by Bruno Rigo Werminghoff on 07/03/20.
+//  Copyright © 2020 Bruno Rigo Werminghoff. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        
+        window = UIWindow()
+        window?.makeKeyAndVisible()
+        
+        let vc = UIViewController()
+        vc.loadViewIfNeeded()
+        vc.view.backgroundColor = .red
+        window?.rootViewController = vc
+        
+        return true
+    }
+
+}
+
