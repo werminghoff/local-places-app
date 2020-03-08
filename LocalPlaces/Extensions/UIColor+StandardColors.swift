8771b57a9515bbcf71c38de465bdbe650fea4247
Bruno Werminghoff <brunowerminghoff@gmail.com>
2020-03-08 01:02:12 -0300

    Added extension to standardize colors

---
+++ LocalPlaces/Extensions/UIColor+StandardColors.swift
@@ -0,0 +1,16 @@
+//
+//  UIColor+StandardColors.swift
+//  LocalPlaces
+//
+//  Created by Bruno Rigo Werminghoff on 08/03/20.
+//  Copyright © 2020 Bruno Rigo Werminghoff. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+    
+    static var titleColor: UIColor { UIColor(red: 0, green: 0, blue: 0, alpha: 1) }
+    static var subtitleColor: UIColor { UIColor(red: 0.6, green: 0.6, blue: 0.6, alpha: 1) }
+    
+}
