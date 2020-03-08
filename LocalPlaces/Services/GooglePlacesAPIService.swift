65f1663a3950e9d895fe555ae2cf8b8d3212463a
Bruno Werminghoff <brunowerminghoff@gmail.com>
2020-03-08 17:42:59 -0300

    Implemented multiple requests joined into a single result to be able to fetch multiple place types from Google Places API

--- LocalPlaces/Services/GooglePlacesAPIService.swift
+++ LocalPlaces/Services/GooglePlacesAPIService.swift
@@ -15,28 +15,55 @@ class GooglePlacesAPIService: NSObject, AbstractPlacesService {
     
     func fetchNear(coordinate: Coordinate, callback: FetchPlacesCallback?) {
         
-        // Add URL parameters
-        let urlParams = [
-            "location": "\(coordinate.latitude),\(coordinate.longitude)",
-            "radius": "500",
-            "key": apiKey,
-        ]
-
-        // Fetch Request
-        AF.request("https://maps.googleapis.com/maps/api/place/nearbysearch/json",
-                   method: .get,
-                   parameters: urlParams)
-            .validate(statusCode: 200..<300)
-            .responseDecodable(of: GooglePlaceModel.Response.self) { (response) in
-                
-                let responseObject = try? response.result.get()
-                
-                if let responseObject = responseObject {
-                    callback?(responseObject.results, nil)
-                } else {
-                    callback?(nil, "Failed to fetch local places")
-                }
+        let types = ["bar" /*, "cafe", "restaurant" */]
+        let group = DispatchGroup()
+        
+        var results: [GooglePlaceModel.Place] = []
+        var hasError: Bool = false
+        
+        for type in types {
+            // Add URL parameters
+            let urlParams = [
+                "location": "\(coordinate.latitude),\(coordinate.longitude)",
+                "radius": "500",
+                "type": type,
+                "key": apiKey,
+            ]
+            
+            // Fetch Request
+            group.enter()
+            AF.request("https://maps.googleapis.com/maps/api/place/nearbysearch/json",
+                       method: .get,
+                       parameters: urlParams)
+                .validate(statusCode: 200..<300)
+                .responseDecodable(of: GooglePlaceModel.Response.self) { (response) in
+                    
+                    let responseObject = try? response.result.get()
+                    
+                    if let responseObject = responseObject {
+                        results.append(contentsOf: responseObject.results)
+                    } else {
+                        hasError = true
+                    }
+                    group.leave()
             }
+        }
+        
+        group.notify(queue: .main) {
+            if hasError {
+                callback?(nil, "Failed to fetch local places")
+            } else {
+                
+                typealias PlaceID = String
+                var uniquePlaces: [PlaceID: GooglePlaceModel.Place] = [:]
+                results.forEach {
+                    uniquePlaces[$0.id] = $0
+                }
+                let places: [AbstractPlace] = uniquePlaces.map({ $0.value })
+                callback?(places, nil)
+            }
+        }
+        
         
     }
     
