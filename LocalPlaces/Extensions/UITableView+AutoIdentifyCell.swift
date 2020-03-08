f6e59ea3daad444cee5274de3a3cda9393c47b5c
Bruno Werminghoff <brunowerminghoff@gmail.com>
2020-03-08 01:01:16 -0300

    Added extensions to simplify registering and dequeueing UITableViewCells

---
+++ LocalPlaces/Extensions/UITableView+AutoIdentifyCell.swift
@@ -0,0 +1,22 @@
+//
+//  UITableView+AutoIdentifyCell.swift
+//  LocalPlaces
+//
+//  Created by Bruno Rigo Werminghoff on 07/03/20.
+//  Copyright © 2020 Bruno Rigo Werminghoff. All rights reserved.
+//
+
+import UIKit
+
+extension UITableView {
+    
+    func register<CellType: UITableViewCell>(_ type: CellType.Type) {
+        register(CellType.self, forCellReuseIdentifier: CellType.defaultIdentifier)
+    }
+    
+    func dequeueReusableCell<CellType: UITableViewCell>(_ type: CellType.Type, for indexPath: IndexPath) -> CellType? {
+        return dequeueReusableCell(withIdentifier: CellType.defaultIdentifier,
+                                   for: indexPath) as? CellType
+    }
+    
+}
---
+++ LocalPlaces/Extensions/UITableViewCellExtension.swift
@@ -0,0 +1,16 @@
+//
+//  UITableViewCellExtension.swift
+//  LocalPlaces
+//
+//  Created by Bruno Rigo Werminghoff on 07/03/20.
+//  Copyright © 2020 Bruno Rigo Werminghoff. All rights reserved.
+//
+
+import UIKit
+
+extension UITableViewCell {
+    
+    /// Use this class' name as default identifier
+    static var defaultIdentifier: String { return String(describing: self) }
+    
+}
